4fdb989e27a01d7655e66a1e135bd957e9acdfa3
sanemat <o.gata.ken@gmail.com>
2015-03-04 17:39:11 +0900

    chore(repository): add comment

--- lib/git_httpsable/push/repository.rb
+++ lib/git_httpsable/push/repository.rb
@@ -2,7 +2,12 @@ module GitHttpsable
   module Push
     class Repository
       def push(remote = 'origin', branch = 'master', options = {})
+        # check current branch's upstream remote and branch
         logger.debug(remote: remote, branch: branch, options: options)
+      end
+
+      def logger
+        ::GitHttpsable::Push.logger
       end
     end
   end
