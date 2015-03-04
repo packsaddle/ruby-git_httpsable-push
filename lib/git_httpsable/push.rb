211ed99ccedb73d10d5e634801cb35309669a00f
sanemat <o.gata.ken@gmail.com>
2015-03-04 16:42:46 +0900

    chore(logger): add logger

--- lib/git_httpsable/push.rb
+++ lib/git_httpsable/push.rb
@@ -1,7 +1,24 @@
+require 'logger'
+
 require 'git_httpsable/push/version'
 
 module GitHttpsable
   module Push
-    # Your code goes here...
+    ISSUE_URL = 'https://github.com/packsaddle/ruby-git_httpsable-push/issues/new'
+    def self.default_logger
+      logger = Logger.new(STDERR)
+      logger.progname = 'GitHttpsable::Push'
+      logger.level = Logger::WARN
+      logger
+    end
+
+    def self.logger
+      return @logger if @logger
+      @logger = default_logger
+    end
+
+    class << self
+      attr_writer :logger
+    end
   end
 end
--- lib/git_httpsable/push/cli.rb
+++ lib/git_httpsable/push/cli.rb
@@ -13,6 +13,28 @@ module GitHttpsable
       def version
         puts "GitHttpsable::Push version #{::GitHttpsable::Push::VERSION}"
       end
+
+      no_commands do
+        def logger
+          ::GitHttpsable::Push.logger
+        end
+
+        def setup_logger(options)
+          if options[:debug]
+            logger.level = Logger::DEBUG
+          elsif options[:verbose]
+            logger.level = Logger::INFO
+          end
+          logger.debug(options)
+        end
+
+        def suggest_messages(options)
+          logger.error 'Have a question? Please ask me:'
+          logger.error ISSUE_URL
+          logger.error 'options:'
+          logger.error options
+        end
+      end
     end
   end
 end
